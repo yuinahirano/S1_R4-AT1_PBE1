3a5b78a7e2b4e02b715778c792ea53d9c7914c74
yuinahirano <nandinhaa1280@gmail.com>
2025-08-19 15:12:26 -0300

    finalizado exercicio 3

---
+++ Exercicio3/app.js
@@ -0,0 +1,52 @@
+const express = require(`express`);
+const app = express();
+const PORT = 8081;
+
+app.get("/operacao/:tipo", (req, res) => {
+    try {
+        const { tipo } = req.params;
+        const { numUm } = req.query;
+        const { numDois } = req.query;
+
+        if (numUm == undefined || numUm == "" || isNaN(numUm) || numDois == undefined || numDois == "" || isNaN(numDois)) {
+            return res.status(400).send(`Campos obrigatórios não preenchidos ou incorretos!`);
+
+        }
+        let resultado = 0;
+
+        switch (tipo) {
+            case "soma":
+                resultado = parseFloat(numUm) + parseFloat(numDois);
+
+                break;
+
+            case "subtracao":
+                resultado = parseFloat(numUm) - parseFloat(numDois);
+                break;
+
+            case "multiplicacao":
+                resultado = parseFloat(numUm) * parseFloat(numDois);
+                break;
+
+            case "divisao":
+                if (numUm == 0 || numDois == 0) {
+                    return res.status(400).send(`Operação inválida.`);
+                } else {
+                    resultado = parseFloat(numUm) / parseFloat(numDois);                   
+                }
+                break;
+
+            default:
+                return res.status(400).send(`Operação inválida.`);
+        }
+        res.status(200).send(`<h1>O resultado da conta é ${resultado}</h1>`);
+
+    } catch (error) {
+        console.error("Erro capturado:", error);
+        res.status(500).send(`Erro interno do servidor`);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
