d4c62e27e026b03d7a823670824015794a63150e
yuinahirano <nandinhaa1280@gmail.com>
2025-08-19 13:13:30 -0300

    codigo da adição feita

--- Exercicio1/app.js
+++ Exercicio1/app.js
@@ -2,6 +2,25 @@ const express = require(`express`);
 const app = express();
 const PORT = 8081;
 
+app.get("/adicao/:numUm/:numDois", (req, res)=>{
+    try {
+        const {numUm} = req.params;
+        const {numDois} = req.params;
+
+        if (numUm == undefined || numUm== "" || isNaN(numUm) || numDois == undefined || numDois == "" || isNaN(numDois)) {
+            return res.status(400).send(`Campos obrigatórios não preenchidos ou incorretos!`);
+
+        } 
+        const soma = parseFloat(numUm) + parseFloat(numDois);
+
+        res.status(200).send(`<h1>A soma dos valores é ${soma}</h1>`);
+
+    } catch (error) {
+        console.error("Erro capturado:", error);
+        res.status(500).send(`Erro interno do servidor`);
+    }
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`Servidor rodando em http://localhost:${PORT}`);
