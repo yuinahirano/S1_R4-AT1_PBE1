4b9aeee8c0b457b54c12992ed6c27f09bd95aa62
yuinahirano <nandinhaa1280@gmail.com>
2025-08-19 16:05:23 -0300

    finalizado exercicio 5

---
+++ Exercicio5/app.js
@@ -0,0 +1,33 @@
+const express = require(`express`);
+const app = express();
+const PORT = 8081;
+
+app.get("/saudacao/:nome", (req, res)=>{
+    try {
+        const {nome} = req.params;
+        const {hora} = req.query;
+
+        if (hora == undefined || hora == "" || isNaN(hora)) {
+            return res.status(400).send(`Campos obrigatórios não preenchidos ou incorretos!`);
+
+        } else if (hora >= 0 && hora < 12) {
+            res.status(200).send(`<h1>Boa dia, ${nome}!</h1>`);
+            
+        } else if (hora >= 12 && hora < 18) {
+            res.status(200).send(`<h1>Boa tarde, ${nome}!</h1>`);
+            
+        } else if (hora >=18 && hora < 23){
+            res.status(200).send(`<h1>Boa noite, ${nome}!</h1>`);
+
+        }
+
+
+    } catch (error) {
+        console.error("Erro capturado:", error);
+        res.status(500).send(`Erro interno do servidor`);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
